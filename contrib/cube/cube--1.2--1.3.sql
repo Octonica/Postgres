57d695d123f73e668fe3c369d001e610d0543d39
Andrey Borodin <amborodin@acm.org>
2017-05-23 15:03:02 +0500

    typo fix

--- contrib/cube/cube--1.2--1.3.sql
+++ contrib/cube/cube--1.2--1.3.sql
@@ -1,7 +1,7 @@
 /* contrib/cube/cube--1.2--1.3.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "ALTER EXTENSION btree_gist UPDATE TO '1.5'" to load this file. \quit
+\echo Use "ALTER EXTENSION cube UPDATE TO '1.3'" to load this file. \quit
 
 
 ALTER OPERATOR FAMILY gist_cube_ops USING gist ADD
